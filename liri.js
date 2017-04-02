5be5e9a45b45bb1fdac61683f970d0f7b8cb2bba
epribs <ericpribramsky@gmail.com>
2017-04-01 23:49:01 -0500

    adding it all

--- liri.js
+++ liri.js
@@ -1,9 +1,11 @@
+var fs = require('fs');
+var spotify = require('spotify');
+var request = require('request');
 var twitterKeys = require("./keys");
 var ck = twitterKeys.twitterKeys.consumer_key;
 var cs = twitterKeys.twitterKeys.consumer_secret;
 var atk = twitterKeys.twitterKeys.access_token_key;
 var ats = twitterKeys.twitterKeys.access_token_secret;
-
 var Twitter = require('twitter');
 var client = new Twitter({
   consumer_key: ck,
@@ -12,21 +14,38 @@ var client = new Twitter({
   access_token_secret: ats
 });
 
-// var params = {screen_name: 'epribs'};
-// client.get('statuses/user_timeline', params, function(error, tweets, response) {
-//   if (!error) {
-//   	var ts = tweets
-//   	console.log(ts.length);
-//   	for (var i = 0; i < ts.length; i++){
-//   		console.log("--------\n" + ts[i].created_at.toString() + "\n" + ts[i].text + "\n");
-//   	}
-//   }
-// });
+
+var twit = function (){
+var params = {screen_name: 'epribs'};
+client.get('statuses/user_timeline', params, function(error, tweets, response) {
+  if (!error) {
+  	var ts = tweets
+  	console.log(ts.length);
+  	for (var i = 0; i < ts.length; i++){
+  		console.log("--------\n" + ts[i].created_at.toString() + "\n" + ts[i].text + "\n");
+  	}
+  }
+});
+}
 
 
-var spotify = require('spotify');
- 
-spotify.search({ type: 'track', query: 'dancing in the moonlight' }, function(err, data) {
+
+var spot = function() {
+var nodeArgs = process.argv;
+var songName = "";
+for (var i = 3; i < nodeArgs.length; i++) {
+  if (i > 3 && i < nodeArgs.length) {
+    songName = songName + "+" + nodeArgs[i];
+  }
+  else {
+    songName += nodeArgs[i];
+  }
+}
+var queryString = {
+	type: "track"
+}
+queryString.query = songName;
+	spotify.search(queryString, function(err, data) {
     if ( err ) {
         console.log('Error occurred: ' + err);
         return;
@@ -41,4 +60,52 @@ spotify.search({ type: 'track', query: 'dancing in the moonlight' }, function(er
 		// The album that the song is from
 		console.log("Album: " + items.album.name)
 });
+}
+
+
+var movie = function() {
+var nodeArgs = process.argv;
+var movieName = "";
+for (var i = 3; i < nodeArgs.length; i++) {
+  if (i > 2 && i < nodeArgs.length) {
+    movieName = movieName + "+" + nodeArgs[i];
+  }
+  else {
+    movieName += nodeArgs[i];
+  }
+}
+var url = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&r=json";
+request(url, function (error, response, data) {
+  console.log('error:', error);
+  console.log('statusCode:', response && response.statusCode);
+  console.log('body:', data.Title);
+});
+}
+
+var appArg = process.argv[2];
+function liri() {
+if (appArg === "my-tweets") {
+	twit();
+} else if (appArg === "spotify-this-song") {
+	spot();
+} else if (appArg === "movie-this") {
+	movie();
+} else if (appArg === "do-what-it-says") {
+	fs.readFile("random.txt", "utf8", function(err, data) {
+		data = data.split(",");
+		var appArg = data[0];
+		var nodeArgs = data[1];
+		liri();
+		return;
+	});
+}
+}
+
+
+
+
+
+
+
+
 
